a78f15033b410776986cd4a6bfb9d2ede112cf67
yaocy2 <cyril.yao@oocl.com>
2020-04-09 00:25:54 +0800

    update url mothed

--- src/main/java/com/cyril/demo/azurewebappdemo/controller/BaseController.java
+++ src/main/java/com/cyril/demo/azurewebappdemo/controller/BaseController.java
@@ -1,5 +1,6 @@
 package com.cyril.demo.azurewebappdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class BaseController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
+    }
+
+    @GetMapping
+    public String root() {
+        return "Hello";
     }
 }
