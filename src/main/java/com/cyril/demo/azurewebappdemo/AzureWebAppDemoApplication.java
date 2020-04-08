f4244bc6a1714ad25216f932246b12290ea565de
yaocy2 <cyril.yao@oocl.com>
2020-04-08 23:42:41 +0800

    update

---
+++ src/main/java/com/cyril/demo/azurewebappdemo/AzureWebAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cyril.demo.azurewebappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureWebAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureWebAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyril/demo/azurewebappdemo/AzureWebAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyril.demo.azurewebappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureWebAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
